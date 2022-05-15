3c0f3701bb22fce776e19415a4d549090c0516c0
atefkbenothman <atefkbenothman@gmail.com>
2022-05-14 17:45:43 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+  print("hello world")
+  test()
+
+def test():
+  assert 1 == 1
+  assert 1 != 2
+
+if __name__ == "__main__":
+  main()
---
+++ readme.md
@@ -0,0 +1,1 @@
+learning how to automate tests in python!
